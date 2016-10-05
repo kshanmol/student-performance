0dc5b4e4e911029ae6293db5c8fc0427e05a1e19
Satyam Kumar <satyam14096@iiitd.ac.in>
2016-10-05 16:28:07 +0530

    Complete k-fold cross validation

--- linear_regression.py
+++ linear_regression.py
@@ -5,21 +5,6 @@ from sklearn.preprocessing import StandardScaler
 import os
 import sys
 
-def find_optimal_ridge_parameter(random_state, low, high, x_test, y_test, x_train, y_train):
-	for i in range(25):
-		temp = [(2*low + high) / 3, (low + 2*high) / 3]
-		regressor = linear_model.SGDRegressor(random_state=14, alpha=temp[0])
-		regressor.fit(x_train, y_train); result = regressor.predict(x_test)
-		val1 = np.mean(map(lambda x, y: (x - y) ** 2, result, y_test))
-
-		regressor = linear_model.SGDRegressor(random_state=14, alpha=temp[1])
-		regressor.fit(x_train, y_train); result = regressor.predict(x_test)
-		val2 = np.mean(map(lambda x, y: (x - y) ** 2, result, y_test))
-
-		if(val1 > val2): low = temp[0]
-		else: high = temp[1]
-	return (low + high) / 2
-
 def process(dataset_file_name, shuffle = False):
 	
 	x, y = [], []
@@ -39,10 +24,36 @@ def process(dataset_file_name, shuffle = False):
 
 	return x, y
 
+def k_fold_cross_validation(random_seed, x, y, delta, k):
+	MSE = 0.0
+	regressor = linear_model.SGDRegressor(random_state=random_seed, alpha=delta)
+	fraction_length = len(x) / k
+	number_of_fractions = (len(x) / fraction_length)
+	for i in range(0, number_of_fractions):
+		x_train = np.concatenate((x[:i * fraction_length], x[(i + 1) * fraction_length :]))
+		y_train = np.concatenate((y[:i * fraction_length], y[(i + 1) * fraction_length :]))
+		x_test = x[i * fraction_length : (i + 1) * fraction_length]
+		y_test = y[i * fraction_length : (i + 1) * fraction_length]
+		
+		regressor.fit(x_train, y_train);
+		result = regressor.predict(x_test)
+		MSE += np.mean(map(lambda x, y: (x - y) ** 2, result, y_test))
+	return MSE / k
 
-def linear_regression(dataset_file_name, train_data_perc):
+def find_optimal_ridge_parameter(random_seed, low, high, x, y, k):
+	for i in range(25):
+		temp = [(2*low + high) / 3, (low + 2*high) / 3]
+		val = []
+		for j in range(2):
+			val.append(k_fold_cross_validation(random_seed, x, y, temp[j], k))
 
-	x,y = process(dataset_file_name, True)
+		if(val[0] > val[1]): low = temp[0]
+		else: high = temp[1]
+	return (low + high) / 2
+
+def linear_regression(dataset_file_name, train_data_perc, random_seed = 14, shuffle = False):
+
+	x, y = process(dataset_file_name, shuffle)
 
 	train_data_length = train_data_perc * len(x) / 100
 	x_train, y_train = x[:train_data_length], y[:train_data_length]
@@ -55,16 +66,17 @@ def linear_regression(dataset_file_name, train_data_perc):
 	x_test = scaler.transform(x_test)
 
 	#SGD
-	random_seed = 30
 	min_alpha, max_alpha = 0.0, 100.0
 
-	opt_alpha = find_optimal_ridge_parameter(random_seed, min_alpha, max_alpha, x_test, y_test, x_train, y_train)
+	opt_alpha = find_optimal_ridge_parameter(random_seed, min_alpha, max_alpha, x_train, y_train, 10)
 	print opt_alpha
-	regressor = linear_model.SGDRegressor(random_state=14, alpha=opt_alpha)
-	regressor.fit(x_train, y_train); result = regressor.predict(x_test)
+	regressor = linear_model.SGDRegressor(random_state=random_seed, alpha=opt_alpha)
+	regressor.fit(x_train, y_train);
+
+	result = regressor.predict(x_test)
 
 	print "SGD solution:"
-	print regressor.coef_
+	# print regressor.coef_
 	print "MSE: ", np.mean(map(lambda x, y: (x - y) ** 2, result, y_test))
 	print "Regressor score: ", regressor.score(x_test, y_test)
 
@@ -74,7 +86,7 @@ def linear_regression(dataset_file_name, train_data_perc):
 	regressor.fit(x_train, y_train); result = regressor.predict(x_test)
 
 	print "Least Squares solution:"
-	print regressor.coef_
+	# print regressor.coef_
 	print "MSE: ", np.mean(map(lambda x, y: (x - y) ** 2, result, y_test))
 	print "Regressor score: ", regressor.score(x_test, y_test)
 
@@ -95,4 +107,4 @@ if __name__ == '__main__':
 
 	file_name = os.path.join(os.path.dirname(__file__), 'data/'+ 'transformed-student' + choice)
 
-	linear_regression(file_name, 95)
+	linear_regression(file_name, 50)
