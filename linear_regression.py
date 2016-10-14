67efb09e6e4ae925a101fd66b93b9aed517ad912
Satyam Kumar <satyam14096@iiitd.ac.in>
2016-10-14 15:51:28 +0530

    Add polynomial and gaussian regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,13 +1,14 @@
-import numpy as np
-import random
-from sklearn import datasets, linear_model
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import KFold, cross_val_score
-import matplotlib.pyplot as plt
-from sklearn.svm import SVR
-
 import os
 import sys
+import random
+
+from sklearn import datasets, linear_model
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import KFold, cross_val_score
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 CONST_RANDOM_SEED = 69
 
@@ -65,50 +66,47 @@ def find_optimal_ridge_parameter(random_seed, x, y, k, penalty):
 	# plt.show()
 	return optimal_delta
 
-def svm(dataset_file_name, train_data_perc, shuffle = False, random_seed = CONST_RANDOM_SEED):
+def get_train_test_data(dataset_file_name, train_data_perc, degree, shuffle = True):
 	x, y = process(dataset_file_name, shuffle)
+
+	poly = PolynomialFeatures(degree = degree, interaction_only = False, include_bias = False)
+	x = poly.fit_transform(x)
 
 	train_data_length = train_data_perc * len(x) / 100
 	x_train, y_train = x[:train_data_length], y[:train_data_length]
 	x_test, y_test = x[train_data_length:], y[train_data_length:]
-
+	
 	# to normalize the data attributes
 	scaler = StandardScaler(); scaler.fit(x_train)
 	
 	x_train = scaler.transform(x_train)
 	x_test = scaler.transform(x_test)
 
-	svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
-	svr_lin = SVR(kernel='linear', C=1e3)
-	svr_poly = SVR(kernel='poly', C=1e3, degree=2)
-	# y_rbf = svr_rbf.fit(x_train, y_train).predict(x_test)
-	# print np.mean(map(lambda x, y: (x - y) ** 2, y_rbf, y_test))
-	y_lin = svr_lin.fit(x_train, y_train).predict(x_test)
-	print np.mean(map(lambda x, y: (x - y) ** 2, y_lin, y_test))
-	# y_poly = svr_poly.fit(x_train, y_train).predict(x_test)
-	# print np.mean(map(lambda x, y: (x - y) ** 2, y_poly, y_test))
+	return x_train, y_train, x_test, y_test
 
-def linear_regression(dataset_file_name, train_data_perc, penalty, k, shuffle = False, random_seed = CONST_RANDOM_SEED):
+def exact_least_squares(dataset_file_name, train_data_perc, degree, shuffle = True):
+	x_train, y_train, x_test, y_test = get_train_test_data(dataset_file_name, train_data_perc, degree, shuffle)
 
-	x, y = process(dataset_file_name, shuffle)
+	regressor = linear_model.LinearRegression()
+	regressor.fit(x_train, y_train);
 
-	train_data_length = train_data_perc * len(x) / 100
-	x_train, y_train = x[:train_data_length], y[:train_data_length]
-	x_test, y_test = x[train_data_length:], y[train_data_length:]
+	result = regressor.predict(x_test)
 
-	# print train_data_perc, len(x_train)
-	
-	# to normalize the data attributes
-	scaler = StandardScaler(); scaler.fit(x_train)
-	
-	x_train = scaler.transform(x_train)
-	x_test = scaler.transform(x_test)
+	print "Least Squares solution:"
+	# print regressor.coef_
+	print "MSE: ", np.mean(map(lambda x, y: (x - y) ** 2, result, y_test))
+	print "Regressor score: ", regressor.score(x_test, y_test)
+
+def linear_regression(dataset_file_name, train_data_perc, penalty, k, degree, max_iter = 5, shuffle = True, random_seed = CONST_RANDOM_SEED):
+
+	x_train, y_train, x_test, y_test = get_train_test_data(dataset_file_name, train_data_perc, degree, shuffle)
 
 	#SGD
 
 	opt_delta = find_optimal_ridge_parameter(random_seed, x_train, y_train, k, penalty)
 	# print opt_delta
-	regressor = linear_model.SGDRegressor(random_state = random_seed, alpha = opt_delta, penalty = penalty)
+
+	regressor = linear_model.SGDRegressor(random_state = random_seed, alpha = opt_delta, penalty = penalty, n_iter = max_iter)
 	regressor.fit(x_train, y_train);
 
 	# result = regressor.predict(x_test)
@@ -118,19 +116,10 @@ def linear_regression(dataset_file_name, train_data_perc, penalty, k, shuffle = 
 	# print "MSE: ", np.mean(map(lambda x, y: (x - y) ** 2, result, y_test))
 	# print "Regressor score: ", regressor.score(x_test, y_test)
 
-	# #Exact Least Squares solution
-
-	# regressor = linear_model.LinearRegression()
-	# regressor.fit(x_train, y_train); result = regressor.predict(x_test)
-
-	# print "Least Squares solution:"
-	# # print regressor.coef_
-	# print "MSE: ", np.mean(map(lambda x, y: (x - y) ** 2, result, y_test))
-	# print "Regressor score: ", regressor.score(x_test, y_test)
-
 	# if interested in RMSE
 	train_error = (np.mean(map(lambda x, y: (x - y) ** 2, regressor.predict(x_train), y_train)) ** 0.5)
 	test_error = (np.mean(map(lambda x, y: (x - y) ** 2, regressor.predict(x_test), y_test)) ** 0.5)
+	print test_error
 	return train_error, test_error
 
 	# # if interested in accuracy
@@ -138,11 +127,12 @@ def linear_regression(dataset_file_name, train_data_perc, penalty, k, shuffle = 
 	# test_score = regressor.score(x_test, y_test)
 	# return train_score, test_score
 
-def plot_learning_curves(dataset_file_name, penalty, k, start = 10, stop = 100, step = 10, random_seed = CONST_RANDOM_SEED):
-	x_axis = [] # train_data_perc
-	y_train, y_test = [], [] # MSE
+def plot_learning_curves(dataset_file_name, penalty, k, degree, max_iter = 5, start = 10, stop = 100, step = 10, shuffle = True, random_seed = CONST_RANDOM_SEED):
+	x_axis = [] 										# train_data_perc
+	y_train, y_test = [], [] 							# MSE
+
 	for train_data_perc in range(start, stop, step):
-		train_error, test_error = linear_regression(file_name, train_data_perc, penalty, k, True)
+		train_error, test_error = linear_regression(file_name, train_data_perc, penalty, k, degree, max_iter, shuffle, random_seed)
 		x_axis.append(train_data_perc)
 		y_train.append(train_error)
 		y_test.append(test_error)
@@ -169,9 +159,12 @@ if __name__ == '__main__':
 		sys.exit(0)		
 
 	file_name = os.path.join(os.path.dirname(__file__), 'data/'+ 'transformed-student' + choice)
-	penalty = sys.argv[2] # l1 for LASSO, l2 for ridge
+	penalty = sys.argv[2] 								# l1 for LASSO, l2 for ridge
+	degree = 1 											# atmost degree of polynomial features
+	k = 10 												# for k-fold cross validation
+	train_data_perc = 50
+	max_iter = 5 										# number of iterations of SGD
 	
-	# linear_regression(file_name, 50, penalty, 10, True)
-	# svm(file_name, 50, True)
+	# linear_regression(file_name, train_data_perc, penalty, k, degree, max_iter)
 	
-	plot_learning_curves(file_name, penalty, 10)
+	plot_learning_curves(file_name, penalty, k, degree, max_iter)
