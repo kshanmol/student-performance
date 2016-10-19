20e1f5bcedf105dda75713e35854d2faa24ebfd4
Satyam Kumar <satyam14096@iiitd.ac.in>
2016-10-19 20:30:24 +0530

    plots, etc

--- linear_regression.py
+++ linear_regression.py
@@ -13,7 +13,7 @@ import get_data
 import numpy as np
 import matplotlib.pyplot as plt
 
-CONST_RANDOM_SEED = 69
+CONST_RANDOM_SEED = 20
 
 def get_train_test_data(dataset_file_name, train_data_perc, degree, shuffle = True, do_PCA = False, PCA_threshold = 0.99):
 	x, y = get_data.process(dataset_file_name, shuffle)
@@ -26,11 +26,10 @@ def get_train_test_data(dataset_file_name, train_data_perc, degree, shuffle = Tr
 	x_test, y_test = x[train_data_length:], y[train_data_length:]
 	
 	if(do_PCA):
-		x_train = get_data.reduced_features(x_train, x_test, PCA_threshold)
+		x_train, x_test = get_data.reduced_features(x_train, x_test, PCA_threshold)
 
 	# to normalize the data attributes
 	scaler = StandardScaler(); scaler.fit(x_train)
-	
 	x_train = scaler.transform(x_train)
 	x_test = scaler.transform(x_test)
 
@@ -50,6 +49,7 @@ def exact_least_squares(dataset_file_name, train_data_perc, degree, shuffle = Tr
 	print "Regressor score: ", regressor.score(x_test, y_test)
 
 def linear_regression(dataset_file_name, train_data_perc, penalty, k, degree, max_iter = 5, shuffle = True, do_PCA = False, random_seed = CONST_RANDOM_SEED):
+	random.seed(random_seed)
 
 	x_train, y_train, x_test, y_test = get_train_test_data(dataset_file_name, train_data_perc, degree, shuffle, do_PCA)
 
@@ -57,24 +57,27 @@ def linear_regression(dataset_file_name, train_data_perc, penalty, k, degree, ma
 
 	regressor = linear_model.SGDRegressor()
 
-	param_grid = [
-		{'alpha': map(lambda x: 2 ** x, range(-10, 10)), 'random_state': [random_seed], 'penalty': [penalty],
-			'n_iter': range(max_iter, max_iter + 1)}
-	]
+	# param_grid = [
+	# 	{'alpha': map(lambda x: 2 ** x, range(-10, 10)), 'random_state': [random_seed], 'penalty': ['l1', 'l2'],
+	# 		'n_iter': range(max_iter, max_iter + 1)}
+	# ]
 
-	best = GridSearchCV(estimator = regressor, param_grid = param_grid, cv = 10)
-	best.fit(x_train, y_train)
+	# best = GridSearchCV(estimator = regressor, param_grid = param_grid, cv = 10)
+	# best.fit(x_train, y_train)
 
-	opt_hyperparameters = best.best_params_
-	print opt_hyperparameters
+	# opt_hyperparameters = best.best_params_
+	opt_hyperparameters = {'penalty': 'l1', 'alpha': 0.0625, 'random_state': random_seed, 'n_iter': 20}
+	# print opt_hyperparameters
 
 	regressor = linear_model.SGDRegressor(**opt_hyperparameters)
 	regressor.fit(x_train, y_train);
 
 	train_error = (np.mean(map(lambda x, y: (x - y) ** 2, regressor.predict(x_train), y_train)) ** 0.5)
 	test_error = (np.mean(map(lambda x, y: (x - y) ** 2, regressor.predict(x_test), y_test)) ** 0.5)
-	# print test_error
-	# return train_error, test_error
+	# print train_data_perc, degree, test_error
+	# print "degree :", degree, "data_perc :", train_data_perc, "test error :", test_error, "train error: ", train_error
+
+	return train_error, test_error
 	
 	# # if interested in accuracy
 	# train_score = regressor.score(x_train, y_train)
@@ -90,20 +93,28 @@ def plot_learning_curves(dataset_file_name, penalty, k, degree, max_iter = 5, st
 			if d not in y_train: y_train[d] = []
 			if d not in y_test: y_test[d] = []
 			train_error, test_error = linear_regression(file_name, train_data_perc, penalty, k, d, max_iter, shuffle, do_PCA, random_seed)
+			print "degree :", d, "data_perc :", train_data_perc, "test error :", test_error, "train error: ", train_error
 			y_train[d].append(train_error)
 			y_test[d].append(test_error)
 		x_axis.append(train_data_perc)
 
+	plt.xlabel("Train Data Percentage")
+	plt.ylabel("Root Mean Square Error")
 	for d in degree:
-		plt.plot(x_axis, y_test[d], label = 'degree-' + str(d))
-	# plt.plot(x_axis, y_test, label = 'test')
-	plt.legend(bbox_to_anchor = (0., 1.02, 1., .102), loc = 3, ncol = 2, mode = "expand", borderaxespad = 0.)
+		plt.plot(x_axis, y_test[d], label = 'test')
+		# plt.plot(x_axis, y_train[d], label = 'train')
+		print x_axis
+		print y_test[d]
+		# print y_train[d]
+
+	
+	if(dataset_file_name.find('mat') != -1): plt.title('Math Course')
+	elif(dataset_file_name.find('por') != -1): plt.title('Portuguese Course')
+	plt.legend(loc='upper right')
+	# plt.legend(bbox_to_anchor = (0., 1.02, 1., .102), loc = 3, ncol = 2, mode = "expand", borderaxespad = 0.)
 	plt.show()
 
 if __name__ == '__main__':
-
-	random.seed(CONST_RANDOM_SEED)
-
 	if(len(sys.argv) == 1):
 		print "Usage:\nFirst Argument:\n0 for mat.csv\n1 for por.csv\nSecond Argument:\nl1 for LASSO\nl2 for Ridge"
 		sys.exit(0)
@@ -120,9 +131,9 @@ if __name__ == '__main__':
 	penalty = sys.argv[2] 								# l1 for LASSO, l2 for ridge
 	degree = 2											# atmost degree of polynomial features
 	k = 10 												# for k-fold cross validation
-	train_data_perc = 50
+	train_data_perc = 80
 	max_iter = 20 										# number of iterations of SGD
 	
-	# linear_regression(file_name, train_data_perc, penalty, k, degree, max_iter)
+	# linear_regression(file_name, train_data_perc, penalty, k, degree, max_iter, do_PCA = True)
 	
 	plot_learning_curves(file_name, penalty, k, [1, 2], max_iter, do_PCA = True)
--- svm.py
+++ svm.py
@@ -10,12 +10,11 @@ from sklearn.svm import SVR
 import numpy as np
 import matplotlib.pyplot as plt
 
-CONST_RANDOM_SEED = 14
+CONST_RANDOM_SEED = 20
 
-def reduction(x):
-	return x
+def svm(dataset_file_name, train_data_perc, k, shuffle = True, do_PCA = False, PCA_threshold = 0.99, random_seed = CONST_RANDOM_SEED):
+	random.seed(random_seed)
 
-def svm(dataset_file_name, train_data_perc, shuffle = True, do_PCA = False, PCA_threshold = 0.99, random_seed = CONST_RANDOM_SEED):
 	x, y = get_data.process(dataset_file_name, shuffle)
 
 	train_data_length = train_data_perc * len(x) / 100
@@ -31,20 +30,23 @@ def svm(dataset_file_name, train_data_perc, shuffle = True, do_PCA = False, PCA_
 	x_train = scaler.transform(x_train)
 	x_test = scaler.transform(x_test)
 
-	param_grid = [
-		{'C': map(lambda x: 2 ** x, range(5, 10)), 'kernel': ['rbf'], 
-			'gamma': map(lambda x: 2 ** x, range(-10, -5)), 'epsilon': map(lambda x: 2 ** x, range(-5, 0))}
-	]
+	# param_grid = [
+	# 	{'C': map(lambda x: 2 ** x, range(5, 10)), 'kernel': ['rbf'], 
+	# 		'gamma': map(lambda x: 2 ** x, range(-10, -5)), 'epsilon': map(lambda x: 2 ** x, range(-5, 0))}
+	# ]
 
 	# param_grid = [
 	# 	{'C': map(lambda x: 2 ** x, range(5, 10)), 'kernel': ['linear'],
 	# 		'epsilon': map(lambda x: 2 ** x, range(-10, -5))}
 	# ]
 
-	best = GridSearchCV(estimator = SVR(), param_grid = param_grid, cv = 5)
-	best.fit(x_train, y_train)
-	opt_hyperparameters = best.best_params_
-	print opt_hyperparameters
+	# best = GridSearchCV(estimator = SVR(), param_grid = param_grid, cv = k)
+	# best.fit(x_train, y_train)
+	# opt_hyperparameters = best.best_params_
+	# print opt_hyperparameters
+
+	opt_hyperparameters = {'epsilon': 0.03125, 'C': 64, 'gamma': 0.0009765625, 'kernel': 'rbf'} # for math
+	# opt_hyperparameters = {'epsilon': 0.5, 'C': 128, 'gamma': 0.0009765625, 'kernel': 'rbf'} # for por
 
 	regressor = SVR(**opt_hyperparameters)
 
@@ -52,16 +54,41 @@ def svm(dataset_file_name, train_data_perc, shuffle = True, do_PCA = False, PCA_
 	
 	result = regressor.predict(x_test)
 	
-	# train_error = (np.mean(map(lambda x, y: (x - y) ** 2, regressor.predict(x_train), y_train)))
-	# test_error = (np.mean(map(lambda x, y: (x - y) ** 2, regressor.predict(x_test), y_test)))
+	train_error = (np.mean(map(lambda x, y: (x - y) ** 2, regressor.predict(x_train), y_train)) ** 0.5)
+	test_error = (np.mean(map(lambda x, y: (x - y) ** 2, regressor.predict(x_test), y_test)) ** 0.5)
 	# print train_error, test_error
-	# return train_error, test_error
+	return train_error, test_error
 	
-	# if interested in accuracy
-	train_score = regressor.score(x_train, y_train)
-	test_score = regressor.score(x_test, y_test)
-	print train_score, test_score
-	return train_score, test_score
+	# # if interested in accuracy
+	# train_score = regressor.score(x_train, y_train)
+	# test_score = regressor.score(x_test, y_test)
+	# print train_score, test_score
+	# return train_score, test_score
+
+def plot_learning_curves(dataset_file_name, k, start = 10, stop = 100, step = 10, shuffle = True, do_PCA = False, random_seed = CONST_RANDOM_SEED):
+	x_axis = [] 										# train_data_perc
+	y_train, y_test = [], []
+
+	for train_data_perc in range(start, stop, step):
+		train_error, test_error = svm(file_name, train_data_perc, k, True, True)
+		print "degree :", "data_perc :", train_data_perc, "test error :", test_error, "train error: ", train_error
+		y_train.append(train_error)
+		y_test.append(test_error)
+		x_axis.append(train_data_perc)
+
+	plt.xlabel("Train Data Percentage")
+	plt.ylabel("Root Mean Square Error")
+	plt.plot(x_axis, y_test, label = 'test')
+	# plt.plot(x_axis, y_train, label = 'train')
+	print x_axis
+	print y_test
+	# print y_train
+	# plt.plot(x_axis, y_test, label = 'test')
+	if(dataset_file_name.find('mat') != -1): plt.title('Math Course')
+	elif(dataset_file_name.find('por') != -1): plt.title('Portuguese Course')
+	plt.legend(loc='upper right')
+	# plt.legend(bbox_to_anchor = (0., 1.02, 1., .102), loc = 3, ncol = 2, mode = "expand", borderaxespad = 0.)
+	plt.show()
 
 if __name__ == '__main__':
 
@@ -86,16 +113,4 @@ if __name__ == '__main__':
 	train_data_perc = 90
 	max_iter = 5 										# number of iterations of SGD
 	
-	x_axis = [] 										# train_data_perc
-	y_train, y_test = [], []
-
-	for train_data_perc in range(30, 80, 10):
-		train_error, test_error = svm(file_name, train_data_perc, True, True)
-		y_train.append(train_error)
-		y_test.append(test_error)
-		x_axis.append(train_data_perc)
-
-	plt.plot(x_axis, y_train, label = 'train')
-	plt.plot(x_axis, y_test, label = 'test')
-	plt.legend(bbox_to_anchor = (0., 1.02, 1., .102), loc = 3, ncol = 2, mode = "expand", borderaxespad = 0.)
-	plt.show()
+	plot_learning_curves(file_name, k, do_PCA = True)
